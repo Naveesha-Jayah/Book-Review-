b2ad4643d425a1088cc755c53a75fea1274bcb7e
Naveesha Jayah <naveeshabjayah@gmail.com>
2024-11-28 17:40:43 +0530

    Finalize

--- frontend/src/Components/AddReview/AddReview.js
+++ frontend/src/Components/AddReview/AddReview.js
@@ -39,7 +39,7 @@ const AddReview = () => {
     <div className="add-review-container">
       <Nav />
       <div className="form-container">
-        <h1 className="form-title">Add Review</h1>
+        <h1 className="form-title">Add Book Review</h1>
 
         {message && <div className="message">{message}</div>}
 
