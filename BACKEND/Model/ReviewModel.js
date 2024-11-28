863d590bc9996d0b5e0ab1ca692c542886503033
Naveesha Jayah <naveeshabjayah@gmail.com>
2024-11-28 11:34:06 +0530

    Second Commit
    Create Models

---
+++ BACKEND/Model/ReviewModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reviewSchema = mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    reviewText: { type: String, required: false },
+    dateAdded: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("ReviewModel", reviewSchema);
--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,1 +1,20 @@
 console.log("hello world");
+//ieJsUI1iUZOXwNuB
+
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+//Middleware
+app.use("/",(req,res,next) => {
+    res.send("It is working");
+})
+
+//DATABASE CONNECTION
+mongoose.connect("mongodb+srv://admin:ieJsUI1iUZOXwNuB@cluster0.g7bx1.mongodb.net/")
+.then(() => console.log("Connected to DB"))
+.then(() => {
+    app.listen(5000);
+})
+.catch((err) => console.log(err));
+
